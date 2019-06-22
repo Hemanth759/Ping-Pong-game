983c50d7bac4dd6c5c81ef58aa413b301294620e
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-06-22 19:04:25 +0530

    Added ball movement and stabilising code for the ball

---
+++ Ping Pong Ball/Assets/Project/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        float choice = Random.Range(0f, 1f);
+        this.GetComponent<Rigidbody2D>().AddForce(new Vector2(choice <= 0.5 ? 40f : -40f, 50f));
+    }
+
+    /// <summary>
+    /// OnCollisionEnter is called when this collider/rigidbody has begun
+    /// touching another rigidbody/collider.
+    /// </summary>
+    /// <param name="other">The Collision data associated with this collision.</param>
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.collider.tag == "Player")
+        {
+            // script for the movement as per the distance of the contact
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(
+                                                            -this.GetComponent<Rigidbody2D>().velocity.x,
+                                                            (other.collider.GetComponent<Rigidbody2D>().velocity.y / 2) + 
+                                                            (this.GetComponent<Rigidbody2D>().velocity.y /3));
+        }
+    }
+}
