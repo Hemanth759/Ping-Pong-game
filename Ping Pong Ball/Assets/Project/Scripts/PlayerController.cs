1a9f28ea7f4518651fd8daa0721d86fd7118e346
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-06-22 15:28:34 +0530

    Added movement for the player

---
+++ Ping Pong Ball/Assets/Project/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Input keys")]
+    public KeyCode upKey;
+    public KeyCode downKey;
+    
+
+    [Space]
+    [Header("Varaibles:")]
+    public float moveMentSpeed;
+
+
+    private Rigidbody2D rb;
+    private Vector2 vector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D> ();
+        if (moveMentSpeed == 0) 
+            moveMentSpeed = 10f;
+        vector.x = 0f;
+        vector.y = moveMentSpeed;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey(upKey)) 
+        {
+            // Debug.Log("going up!!");
+            rb.velocity = vector;
+        } 
+        else if(Input.GetKey(downKey))
+        {
+            // Debug.Log("going down!!");
+            rb.velocity = -vector;
+        }
+        else
+        {
+            rb.velocity = 0 * vector;
+        }        
+    }
+}
