075328ca1cb21431a8390ebef50203896d985e93
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-06-29 22:48:34 +0530

    Added menu to the scenes

---
+++ Ping Pong Ball/Assets/Project/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void onPressed() 
+    {
+        string buttonName = this.name;
+        switch (buttonName)
+        {
+            case "PlayButton":
+                SceneManager.LoadScene(1);
+                break;
+            case "Settings":
+                Debug.Log("haven't done the settings page yet");
+                break;
+            default:
+                Application.Quit();
+                break;
+        }
+    }
+}
